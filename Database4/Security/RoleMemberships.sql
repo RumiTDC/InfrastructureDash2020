588d3dd454359e9fe0778573cffd312f619f42e6
Rune Mikkelsen <rumi@tdc.dk>
2020-02-04 10:49:22 +0100

    Cleanup

---
+++ Database4/Security/RoleMemberships.sql
@@ -0,0 +1,14 @@
+﻿ALTER ROLE [db_owner] ADD MEMBER [m88954];
+
+
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [m88954];
+
+
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [m88954];
+
+
+GO
+ALTER ROLE [db_accessadmin] ADD MEMBER [m88954];
+
